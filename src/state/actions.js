e67401cedbabc573bcf7e6c9bc96d67d1e30f636
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-08-22 14:59:35 +0200

    Don't send pointless requests

--- src/state/actions.js
+++ src/state/actions.js
@@ -6,74 +6,7 @@ export const USER_REQUEST = "USER_REQUEST";
 export const USER_SUCCESS = "USER_SUCCESS";
 export const USER_FAILURE = "USER_FAILURE";
 
-    /* USING REDUX THUNK WITHOUT CUSTOM API MIDDLEWARE */
-
-// function questionsRequest() {
-//   return {
-//     type: QUESTIONS_REQUEST
-//   }
-// }
-// function questionsSuccess(payload) {
-//   return {
-//     type: QUESTIONS_SUCCESS,
-//     payload
-//   };
-// }
-// function questionsFailure(payload) {
-//   return {
-//     type: QUESTIONS_FAILURE,
-//     payload
-//   }
-// }
-
-// export function fetchQuestions() {
-//   return function (dispatch, getState) {
-//     dispatch(questionsRequest());
-//     return fetch("/api/questions")
-//       .then((res) => res.json())
-//       .then((res) => {
-//         if (res.success) {
-//           return dispatch(questionsSuccess(res));
-//         }
-//         return dispatch(questionsFailure(res));
-//       });
-//   };
-// }
-// function userRequest() {
-//   return {
-//     type: USER_REQUEST,
-//   };
-// }
-// function userSuccess(payload) {
-//   return {
-//     type: USER_SUCCESS,
-//     payload,
-//   };
-// }
-// function userFailure(error) {
-//   return {
-//     type: USER_FAILURE,
-//     error,
-//   };
-// }
-
-// function fetchUser(id) {
-//   return function (dispatch) {
-//     dispatch(userRequest());
-//     return fetch(`/api/users/${id}`)
-//       .then((res) => res.json())
-//       .then((res) => {
-//         if (res.success) {
-//           return dispatch(userSuccess(data));
-//         }
-//         return dispatch(userFailure(data));
-//       });
-//   };
-// }
-
-    /* USING CUSTOM API MIDDLEWARE */
-
-export function fetchQuestions(onSuccess = null) {
+function fetchQuestions(onSuccess) {
   return {
     [CALL_API]: {
       endpoint: "/api/questions",
@@ -83,7 +16,20 @@ export function fetchQuestions(onSuccess = null) {
   };
 }
 
-export function fetchUser(userId, onSuccess = null) {
+export function loadQuestions(onSuccess = null) {
+  // this is possible because I use redux-thunk middleware
+  return function(dispatch, getState) {
+    const currentState = getState();
+    // don't make pointless requests
+    if (currentState.questions.length !== 0) {
+      return null;
+    }
+    // else dispatch fetchQuestions action
+    return dispatch(fetchQuestions(onSuccess))
+  } 
+}
+
+function fetchUser(userId, onSuccess) {
   return {
     [CALL_API]: {
       endpoint: `/api/users/${userId}`,
@@ -91,4 +37,16 @@ export function fetchUser(userId, onSuccess = null) {
       onSuccess,
     },
   };
+}
+
+export function loadUser(userId, onSuccess = null) {
+  return function(dispatch, getState) {
+    const currentState = getState();
+    // don't make pointless requests
+    if (currentState.users.find(item => item.user_id === userId)) {
+      return null;
+    }
+    // else dispatch fetchUser action
+    return dispatch(fetchUser(userId, onSuccess))
+  } 
 }
